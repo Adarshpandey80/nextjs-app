1f7272db2c142aa8a2b2fa4a2e854362ea85368f
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-02-02 22:15:42 +0530

    add  external css

--- app/about/Header.tsx
+++ app/about/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Style from "./Header.module.css"
 
 function Header() {
   return (
   <>
-  <h1> About page 2</h1>
+  <h1 className={Style.h1}> About page 2</h1>
   </>
   )
 }

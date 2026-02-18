d1ae14b39dabe500ade7aa50fb93704cbb0b6a72
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-02-19 00:45:55 +0530

    import css file and use next Image

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,22 @@ import Image from "next/image";
 import Card from "./Card";
 
 
+import style from './layout.module.css'
+
 export default function Home() {
   return (
    <>
     <Card/>
-    <h1>welcome next app</h1>
-
+    <h1 className={style.h1}>welcome next app</h1>
+    {/* <img className={style.img} src="/images/pumaw1.avif" alt="shoes photo" /> */}
+    <div>
+        <Image src="/images/pumaw1.avif"
+        width={400}
+        height={400}
+        
+         alt="my photo"/>
+    </div>
+  
    </>
   );
 }

a3c134a011335d3288f470355b2683a92b6477f4
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-07-08 23:37:04 +0530

    next/head

--- app/Admin/Footer.jsx
+++ app/Admin/Footer.jsx
@@ -1,8 +1,24 @@
+// complete this 
+
 import React from 'react'
+import Head from 'next/head'
 
 function Footer() {
   return (
-    <div>Footer</div>
+   <>
+    <Head>
+  <title style={{ color: 'black' }}>Footer</title>
+    </Head>
+    <body style={{ backgroundColor: 'lightblue' }}>
+    <h1 style={{ color: 'black' }}>Footer section here </h1>
+     <p style={{ color: 'black' }}>This is the footer content.</p>
+</body> 
+
+
+    
+
+   </>
+    
   )
 }
 
--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Homepage from './Homepage'
+import FristPost from './FirstPost'
 function page() {
   return (
     <>
      
      <Homepage />
+     <FristPost />
     </>
   )
 }

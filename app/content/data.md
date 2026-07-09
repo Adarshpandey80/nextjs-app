87363140958e8c7c726af9bccfd5540a9a92bf5a
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-07-09 23:58:23 +0530

    use multipe md file

---
+++ app/content/data.md
@@ -0,0 +1,8 @@
+
+---
+title: This is data file
+author: Adarsh
+date: 2026-07-09
+---
+
+here is some data in data file. Data file is used to store data that can be imported and used in other components or pages. This allows for better organization and separation of concerns in your Next.js application.
--- app/content/page.tsx
+++ app/content/page.tsx
@@ -1,23 +1,63 @@
 import matter from 'gray-matter';
 import fs from 'fs';
+import path from 'path';
 
 
-const file = fs.readFileSync("blog.md", "utf8");
-const { data, content } = matter(file);
 
-console.log(data);
-console.log(content);
+// const filePath = path.join(process.cwd(), "app","content", 'blog.md');
+// const file = fs.readFileSync(filePath, "utf8");
+// const { data, content } = matter(file);
+
+const folderPath = path.join(process.cwd() , "app", "content");
+
+const files = fs.readdirSync(folderPath);
+
+console.log(files);
+
+const blogs = files.map((fileName) =>{
+   const   filePath = path.join( folderPath , fileName);
+   const filecontent  = fs.readFileSync(filePath, "utf8");
+   const {data , content} = matter(filecontent);
+   return {
+      slug : fileName.replace('.md', ''),
+      ...data,
+      content
+   };
+})
 
 
-import React from 'react'
 
-function page() {
+
+function Page() {
   return (
     <>
-  <p>Title: {data.title}</p>
+  
+    <h1>Content Page</h1>
+
+   {/* <div className='m-4 p-4 border-2 border-blue-950 rounded-lg shadow-md'>
+      <h2 className='text-2xl font-semibold mb-2'>{data.title}</h2>
+      <h2 className='text-2xl font-semibold mb-2'>{data.author}</h2>
+      <p className='text-gray-700 mb-4'>{data.date.toString()}</p>
+      <div className='text-gray-800'>
+        {content}
+      </div>
+    </div>   */}
+
+
+    { blogs.map((blog) => (
+      <div key={blog.slug} className='m-4 p-4 border-2 border-blue-950 rounded-lg shadow-md'>
+        <h2 className='text-2xl font-semibold mb-2'>{blog.title}</h2>
+        <h2 className='text-2xl font-semibold mb-2'>{blog.author}</h2>
+        <p className='text-gray-700 mb-4'>{blog.date}</p>
+        <div className='text-gray-800'>
+          {blog.content}
+        </div>
+      </div>
+    ))}
+
 
     </>
   )
 }
 
-export default page
+export default Page

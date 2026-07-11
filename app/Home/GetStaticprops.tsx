ece647662f23878f52ed7fb2120abe1bd5ecb775
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-07-11 16:17:27 +0530

    GetStaticProps

---
+++ app/Home/GetStaticprops.tsx
@@ -0,0 +1,10 @@
+export default function GetStaticprops() {
+  const name = "Adarsh Pandey";
+
+  return (
+    <div>
+      <h1>Name {name}</h1>
+    </div>
+  );
+}
+
--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Homepage from './Homepage'
 import FristPost from './FirstPost'
+ import GetStaticprops from './GetStaticprops'
 function page() {
   return (
     <>
      
      <Homepage />
      <FristPost />
+      <GetStaticprops />
     </>
   )
 }
